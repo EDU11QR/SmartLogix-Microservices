11677caaf1ae2deace0431efc85a70bef631849b
DevEdu <quisperojasedu@gmail.com>
2026-05-31 20:11:57 -0500

    feat: integracion y comunicacion entre microservicios con Eureka

--- pedido-service/src/main/java/com/edudev/pedido_service/PedidoController.java
+++ pedido-service/src/main/java/com/edudev/pedido_service/PedidoController.java
@@ -1,12 +1,17 @@
 package com.edudev.pedido_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class PedidoController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${server.port}")
     private String puerto;
@@ -14,15 +19,31 @@ public class PedidoController {
     @GetMapping("/pedido")
     public String registrarPedido(@RequestParam String cliente,
                                   @RequestParam String producto,
-                                  @RequestParam int cantidad) {
+                                  @RequestParam int cantidad,
+                                  @RequestParam double precio) {
 
-        return "Pedido registrado correctamente | Cliente: "
-                + cliente
-                + " | Producto: "
-                + producto
-                + " | Cantidad: "
-                + cantidad
-                + " | Puerto: "
+        String stock = restTemplate.getForObject(
+                "http://inventario-service/stock?producto=" + producto,
+                String.class
+        );
+
+        String operacion = restTemplate.getForObject(
+                "http://operaciones-service/calcular?precio="
+                        + precio
+                        + "&cantidad="
+                        + cantidad,
+                String.class
+        );
+
+        return "PEDIDO PROCESADO\n\n"
+                + "Cliente: " + cliente
+                + "\nProducto: " + producto
+                + "\nCantidad: " + cantidad
+                + "\n\n=== INVENTARIO ===\n"
+                + stock
+                + "\n\n=== OPERACIONES ===\n"
+                + operacion
+                + "\n\nPuerto Pedido Service: "
                 + puerto;
     }
 }
